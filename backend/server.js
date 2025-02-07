e76bc508f08989bbb615d5c25c1d2d0cdc02a3cc
lilen <lilenbidkar@gmail.com>
2025-02-07 19:37:59 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,27 @@ const app = express();
 const PORT = 8080;
 const link = "http://127.0.0.1:8080/";
 
+app.use((req, res, next) => {
+    console.log(`hostname: ${req.hostname}, URL: ${req.url}, Method: ${req.method}`);
+    next();
+})
+
+
 app.get("/", (req, res) => {
     res.send("Hello");
 });
+
+app.get("/about", (req, res) => {
+    fs.readFile('../model/user.json', 'utf8', (err, data) => {
+        if(err) {
+            console.log("Error: ", err);
+            res.status(404).send("404 Page Not Found");
+        } else {
+            const jsonData = JSON.parse(data);
+            res.json(jsonData.credentials[0]);
+        }
+    })
+})
 
 
 app.listen(PORT, () => {
