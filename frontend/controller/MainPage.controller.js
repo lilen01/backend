c4de7bf413b21be759955bd87ca9cd55119d8241
lilen <lilenbidkar@gmail.com>
2025-02-14 01:32:05 +0530

    added post new User

--- frontend/controller/MainPage.controller.js
+++ frontend/controller/MainPage.controller.js
@@ -82,6 +82,60 @@ sap.ui.define([
               });              
         },
 
+        addNewUser: function() {
+            // var oPath = oEvent.getSource().getBindingContext('userData').getPath();
+            // console.log(oPath);
+
+            if (!this.oDialog) {
+                this.loadFragment({
+                    name: "vendor.fragment.NewDialog"
+                }).then(function(odialog) {
+                    this.oDialog = odialog;
+                    this.oDialog.open();
+                    // this.oDialog.bindElement({
+                    //     path: oPath,
+                    //     model: 'userData'
+                    // })
+                }.bind(this));
+            } else {
+                this.oDialog.open();
+                // this.oDialog.bindElement({
+                //     path: oPath,
+                //     model: 'userData'
+                // })
+            }
+        },
+
+        onCloseDialog : function() {
+            this.oDialog.close();
+        },
+
+        onSubmitUserData: function(oEvent) {
+            var username = this.byId("idUsername").getValue();
+            var email = this.byId("idEmail").getValue();
+            var password = this.byId("idPassword").getValue();
+
+            var oData = {
+                userName : username,
+                password : password,
+                email : email
+            };
+
+            fetch(url, {
+                method : "POST",
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body : JSON.stringify(oData)
+            })
+                .then(response => response.json())
+                .then(() => {
+                    console.log("User details entered successfully.");
+                })
+                .catch(err => console.log("Error while creating user: ", err));
+
+        },
+
         toLoginPage : function () {
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.navTo("LoginPage");
